f5a0ae4144bdf8bc3489ffc7b43ac0d25302e50d
Berkaygunn <berkay2gun@gmail.com>
2023-01-30 22:48:38 +0300

    task yenis

--- src/ahmet.java
+++ src/ahmet.java
@@ -2,5 +2,6 @@ public class ahmet {
     public static void main(String[] args) {
         System.out.println("TEST LEAD YALVARIRIM KABUL ET Bunu :D");
         System.out.println("sssss");
+        System.out.println("yeni  test lead Ahmet Canerdir ");
     }
 }
